fac57c5ce05aae79c8385a7687520b455a91bb7f
BPitts8019 <bradleyj.pitts@gmail.com>
2019-08-22 19:39:16 -0700

    Updated NumberButtons to use state variables

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const NumberButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+const NumberButton = ({label, currentNum, setNumber}) => {
+   const updateNumber = () => {
+      setNumber(currentNum + label);
+   };
+   
+   return (
+      <>
+         {/* Display a button element rendering the data being passed down from the parent container on props */}
+         <button onClick={updateNumber}>{label}</button>
+      </>
+   );
 };
+
+export default NumberButton;
