2ac243468193a9cc585c184fd31d8daa461679eb
BPitts8019 <bradleyj.pitts@gmail.com>
2019-08-22 22:33:44 -0700

    Started CSS; consider using preprocessor

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}12.09876543</div>;
+  return <div className="code">{/* Display any props data here */}12.09876543</div>;
 };
 
 export default Display;
