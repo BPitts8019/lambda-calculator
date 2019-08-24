7460c9a98528800db11c3aed236a2505964ade2f
BPitts8019 <bradleyj.pitts@gmail.com>
2019-08-23 17:34:31 -0700

    Added button styles and colors; working on placement

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Display = () => {
-  return <div className="code">{/* Display any props data here */}12.09876543</div>;
+  return <div className="code">{/* Display any props data here */}12.09876543123</div>;
 };
 
 export default Display;
