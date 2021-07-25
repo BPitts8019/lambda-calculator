d0a1c0052e8005eaef8666263374dccaf4f477c7
BPitts8019 <bradleyj.pitts@gmail.com>
2021-07-25 16:05:31 -0700

    Map Operators in its wrapper component

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Button from "../Button";
 
-const OperatorButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+const OperatorButton = ({ operator: { char, value } }) => {
+   return (
+      <>
+         {/* Display a button element rendering the data being passed down from the parent container on props */}
+         <Button className="button-op" char={char} value={value} />
+      </>
+   );
 };
+
+export default OperatorButton;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
+import OperatorButton from "./OperatorButton";
 
 //Import your array data to from the provided data file
 import { operators } from "../../../data";
@@ -11,9 +12,17 @@ const Operators = () => {
 
    return (
       <div>
-         {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+         {
+            /* STEP 3 - Use .map() to iterate over your array data and return a button
+            component matching the name on the provided file. Pass
+            it any props needed by the child component*/
+
+            operatorState.map((operator, index) => (
+               <OperatorButton key={"op-" + index} operator={operator} />
+            ))
+         }
       </div>
    );
 };
+
+export default Operators;
